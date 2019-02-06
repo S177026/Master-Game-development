fb793166cfd4b862991e0dae3b486b68707abbd6
Lewis <S177026@ucs.ac.uk>
2019-02-06 17:27:15 +0000

    Research and Project

---
+++ Assessments/12 Week Game/Survival Group Project/Assets/Resources.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resources : MonoBehaviour
+{
+    public int maxStoneResource;
+    public int maxWaterResource;
+    public int maxHerbResource;
+    public int maxWoodResource;
+
+    public int currentWood;
+    public int currentWater;
+    public int currentHerb;
+    public int currentStone;
+
+    [Header("Current Resources")]
+    public Text stone;
+    public Text wood;
+    public Text Herb;
+    public Text water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHerb = 0;
+        currentStone = 0;
+        currentWater = 0;
+        currentWood = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stone.text = ("Stone: " + currentStone);
+        wood.text = ("Wood: " + currentWood);
+        Herb.text = ("Herb: " + currentHerb);
+        water.text = ("Water: " + currentWater);
+    }
+
+
+}
